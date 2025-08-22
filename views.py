fe8b625919b4ad1bf9b1884524689a39cf3a59f5
dni ⚡ <office@dnilabs.com>
2025-08-22 22:50:02 +0200

    feat: add public page for switch (#33)
    * feat: add public page for switch

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Depends, Request
+from http import HTTPStatus
+
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.helpers import template_renderer
+
+from .crud import get_bitcoinswitch
 
 bitcoinswitch_generic_router = APIRouter()
 
@@ -16,4 +20,18 @@ async def index(request: Request, user: User = Depends(check_user_exists)):
     return bitcoinswitch_renderer().TemplateResponse(
         "bitcoinswitch/index.html",
         {"request": request, "user": user.json()},
+    )
+
+
+@bitcoinswitch_generic_router.get("/public/{switch_id}", response_class=HTMLResponse)
+async def public(
+    switch_id: str, request: Request, user: User = Depends(check_user_exists)
+):
+    switch = await get_bitcoinswitch(switch_id)
+    if not switch:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Switch not found")
+
+    return bitcoinswitch_renderer().TemplateResponse(
+        "bitcoinswitch/public.html",
+        {"request": request, "user": user.json(), "switch": switch.json()},
     )
